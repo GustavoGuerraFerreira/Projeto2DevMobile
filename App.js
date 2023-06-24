ec67205210150c19d5e8856cec9b58784b558612
Gustavo <gutvasco123@gmail.com>
2023-06-23 23:27:48 -0300

    change import

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import ItensPage from './src/pages/MoviesPage';
-import PublicScreens from './src/routes/PublicScreens';
 import Routes from './src/routes';
 import AuthRoutes from './src/routes/authRoutes';
 import { AuthProvider } from './src/contexts/AuthContext';
